d7fd6d62e989f0870b8a5b31c88376c7711d1290
panhezeng <panhezeng@gmail.com>
2021-11-22 11:21:06 +0800

    feat: 事件

--- src/components/example/TemplateComp.vue
+++ src/components/example/TemplateComp.vue
@@ -12,89 +12,12 @@
   </view>
 </template>
 
-<!--<script lang="ts">-->
-<!--import {-->
-<!--  defineComponent,-->
-<!--  ref,-->
-<!--  PropType,-->
-<!--  onMounted,-->
-<!--  getCurrentInstance,-->
-<!--  SetupContext,-->
-<!--} from "vue";-->
-
-<!--import { useStore } from "vuex";-->
-<!--import { storeKey } from "@/store";-->
-
-<!--import Taro from "@tarojs/taro";-->
-
-<!--import { Todo } from "src/components/example/models";-->
-
-<!--function useClickCount() {-->
-<!--  const clickCount = ref(0);-->
-
-<!--  function increment() {-->
-<!--    clickCount.value += 1;-->
-<!--    return clickCount.value;-->
-<!--  }-->
-
-<!--  return { clickCount, increment };-->
-<!--}-->
-
-<!--function useDisplayTodo(todos: Ref<Todo[]>) {-->
-<!--  const todoCount = computed(() => todos.value.length);-->
-<!--  return { todoCount };-->
-<!--}-->
-
-<!--interface User {-->
-<!--  firstName: string;-->
-<!--  lastName: string;-->
-<!--}-->
-
-<!--export default defineComponent({-->
-<!--  components: {},-->
-<!--  props: {-->
-<!--    user: {-->
-<!--      type: Object as PropType<User>,-->
-<!--      required: true,-->
-<!--    },-->
-<!--    todos: {-->
-<!--      type: Array as PropType<Todo[]>,-->
-<!--      default: () => [] as Todo[],-->
-<!--    },-->
-<!--  },-->
-<!--  emits: ["template-comp-danger"],-->
-<!--  /* eslint-disable @typescript-eslsed-vars,no-unused-vars */-->
-<!--  setup(props, context) {-->
-<!--    const internalInstac  eCretntance()!;-->
-<!--    const publicInstance = internalInstance.proxy;-->
-<!--    const store = useStore(storeKey);-->
-<!--    /* eslint-disable @typescript-eslint/no-unused-vars,no-unused-vars */-->
-
-<!--    function getElement() {-->
-<!--      return Taro.createSelectorQuery().select(".example-template-comp");-->
-<!--    }-->
-<!--    // Vue 的虚拟 DOM 树渲染为 Taro 的虚拟 DOM 之后触发 ，这时无法通过 createSelectorQuery 等方法获取小程序渲染层 DOM 节点-->
-<!--    onMounted(() => {-->
-<!--      const taroInstance = Taro.getCurrentInstance();-->
-<!--      const router = taroInstance.router!;-->
-<!--      Taro.nextTick(() => {-->
-<!--        // console.log("nextTick", !!getElement());-->
-<!--      });-->
-<!--      Taro.eventCenter.on(router.onReady, () => {-->
-<!--        // console.log("onReady", !!getElement());-->
-<!--      });-->
-<!--      Taro.eventCenter.on(router.onShow, () => {-->
-<!--        // console.log("onShow", !!getElement());-->
-<!--      });-->
-<!--    });-->
-
-<!--    return {-->
-<!--      ...useClickCount(context),-->
-<!--      ...useDisplayTodo(toRef(props, "todos"))-->
-<!--    };-->
-<!--  },-->
-<!--});-->
-<!--</script>-->
+<script lang="ts">
+export const eventName = {
+  danger: "template-comp-danger",
+};
+export default {};
+</script>
 
 <script setup lang="ts">
 import {
@@ -151,14 +74,14 @@ const props = defineProps({
   },
 });
 
-const emit = defineEmits(["template-comp-danger"]);
+const emit = defineEmits(Object.values(eventName));
 
 const { clickCount, increment } = useClickCount();
 
 const { todoCount } = useDisplayTodo(toRef(props, "todos"));
 
 function danger(event: Event) {
-  emit("template-comp-danger", event);
+  emit(eventName.danger, event);
   // console.log(event:Event);
 }
 
